08cca7d10487d47fcc10a1f04fb5291aff35f048
Dasha <darapodkidyseva@gmail.com>
2025-06-27 17:17:20 +0400

    first commit

---
+++ src/main/java/ru/neoflex/autoplanner/AutoplannerApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.autoplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/neoflex/autoplanner/AutoplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.autoplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
